d23037e805268ea1a32ba52c49e8446bb74df600
Genta23 <konno.g.aa@m.titech.ac.jp>
2023-09-19 03:03:49 +0900

    0918

---
+++ atcoder/ABC216/d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <string>
+#include <math.h>
+#include <cmath>
+#include <bitset>
+using namespace std;
+typedef long long ll;
+const int inf = 1e9;
+#define rep(i,n) for (int i=0; i<n; i++)
+
+int main(){
+
+    return 0;
+}
