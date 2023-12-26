3cb5ed930d48e9cdaf0cfe4101221655d3840a2d
Genta23 <konno.g.aa@m.titech.ac.jp>
2023-12-26 10:26:44 +0900

    add

---
+++ atcoder_coding/Segment_tree/abc125c.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <math.h>
+#include <cmath>
+#include <bitset>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+#include <functional>
+using namespace std;
+typedef long long ll;
+const int inf = 1e9;
+const ll infl = 1e18;
+#define rep(i,n) for (int i=0; i<n; i++)
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+template<typename T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> bool checker(T s, T t, T s__, T t__){ return ((s >= 0 && t >= 0 && s < s__ && t < t__) ? true : false); }
+using Graph = vector<vector<int>>;
+/* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
+    update(a,b,x): 区間[a,b) の要素を x に更新。O(log(n))
+    query(a,b): [a,b) での最小の要素を取得。O(log(n))
+*/
+/* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
+    update(i,x): i 番目の要素を x に更新。O(log(n))
+    query(a,b): [a,b) での最小の要素を取得。O(log(n))
+*/
+template <typename T>
+struct RMQ {
+    const T INF = numeric_limits<T>::max();
+    int n;         // 葉の数
+    vector<T> dat; // 完全二分木の配列
+    RMQ(int n_) : n(), dat(n_ * 4, INF) { // 葉の数は 2^x の形
+        int x = 1;
+        while (n_ > x) {
+            x *= 2;
+        }
+        n = x;
+    }
+
+    void update(int i, T x) {
+        i += n - 1;
+        dat[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2;  // parent
+            dat[i] = gcd(dat[i * 2 + 1], dat[i * 2 + 2]);
+        }
+    }
+
+    // the minimum element of [a,b)
+    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+    T query_sub(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) {
+            return INF;
+        } else if (a <= l && r <= b) {
+            return dat[k];
+        } else {
+            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return gcd(vl, vr);
+        }
+    }
+};
+
+int main(){
+    int n; cin >> n;
+    RMQ<int> rmq(n);
+
+    rep(i, n){
+        int a; cin >> a;
+        
+    }
+
+    return 0;
+}
---
+++ atcoder_coding/Segment_tree/rmq_class.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <math.h>
+#include <cmath>
+#include <bitset>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+#include <iomanip>
+#include <functional>
+using namespace std;
+typedef long long ll;
+const int inf = 1e9;
+const ll infl = 1e18;
+#define rep(i,n) for (int i=0; i<n; i++)
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+template<typename T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> bool checker(T s, T t, T s__, T t__){ return ((s >= 0 && t >= 0 && s < s__ && t < t__) ? true : false); }
+using Graph = vector<vector<int>>;
+/* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
+    update(a,b,x): 区間[a,b) の要素を x に更新。O(log(n))
+    query(a,b): [a,b) での最小の要素を取得。O(log(n))
+*/
+template <typename T>
+class RMQ {
+public:
+    const T INF = numeric_limits<T>::max();
+    int n;
+    vector<T> dat, lazy;
+    RMQ(int n_) : n(), dat(n_ * 4, INF), lazy(n_ * 4, INF) {
+        int x = 1;
+        while (n_ > x) x *= 2;
+        n = x;
+    }
+
+    void update(int a, int b, T x, int k, int l, int r) {
+        eval(k);
+        if (a <= l && r <= b) {  // 完全に内側の時
+            lazy[k] = x;
+            eval(k);
+        } else if (a < r && l < b) {                     // 一部区間が被る時
+            update(a, b, x, k * 2 + 1, l, (l + r) / 2);  // 左の子
+            update(a, b, x, k * 2 + 2, (l + r) / 2, r);  // 右の子
+            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+            }
+    }
+    void update(int a, int b, T x) { update(a, b, x, 0, 0, n); }
+
+    T query_sub(int a, int b, int k, int l, int r) {
+        eval(k);
+        if (r <= a || b <= l) {  // 完全に外側の時
+            return INF;
+        } else if (a <= l && r <= b) {  // 完全に内側の時
+            return dat[k];
+        } else {  // 一部区間が被る時
+            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return min(vl, vr);
+        }
+    }
+    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+
+private:
+    /* lazy eval */
+    void eval(int k) {
+        if (lazy[k] == INF) return;  // 更新するものが無ければ終了
+        if (k < n - 1) {             // 葉でなければ子に伝搬
+            lazy[k * 2 + 1] = lazy[k];
+            lazy[k * 2 + 2] = lazy[k];
+        }
+        // 自身を更新
+        dat[k] = lazy[k];
+        lazy[k] = INF;
+    }
+};
+
+int main(){
+    int n, q; cin >> n >> q;
+    RMQ<int> rmq(n);
+
+    rep(i, q){
+        int tmp; cin >> tmp;
+        if(tmp == 0){
+            int s, t, x; cin >> s >> t >> x;
+            rmq.update(s, t+1, x);
+        }
+        else{
+            int s, t; cin >> s >> t;
+            cout << rmq.query(s, t+1) << endl;
+        }
+    }
+    return 0;
+}
