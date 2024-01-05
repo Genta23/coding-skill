f9ce3290946750f94ca2f7095d617c4a7cfe2f5f
Genta23 <konno.g.aa@m.titech.ac.jp>
2024-01-06 00:12:34 +0900

    add

--- atcoder_coding/ABC331/f.cpp
+++ atcoder_coding/ABC331/f.cpp
@@ -22,8 +22,65 @@ template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, tr
 template<typename T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
 template<typename T> bool checker(T s, T t, T s__, T t__){ return ((s >= 0 && t >= 0 && s < s__ && t < t__) ? true : false); }
 using Graph = vector<vector<int>>;
+int mod[5] = {998244353, 1000000007, 1000000009, 1000000021, 1000000033};
+
+struct Data { ll htor, htol, b; int m; };
+
+template <typename T> class RMQ {
+public:
+    const T INF = numeric_limits<T>::max();
+    int n;         // 葉の数
+    vector<T> dat; // 完全二分木の配列
+    RMQ(int n_) : n(), dat(n_ * 4, INF) { // 葉の数は 2^x の形
+        int x = 1;
+        while (n_ > x) x *= 2;
+        n = x;
+    }
+
+    void update(int i, T x) {
+        i += n - 1;
+        dat[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2;  // parent
+            T vl = dat[i * 2 + 1], vr = dat[i * 2 + 2];
+            dat[i] = {(vl.htor * vr.b + vr.htor) % vl.m, (vl.htol + vr.htol * vl.b) % vl.m, (vl.b * vr.b) % vl.m, vl.m};
+        }
+    }
+
+    // the minimum element of [a,b)
+    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
+    T query_sub(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) return {0, 0, 1, dat[k].m}; // 範囲外
+        else if (a <= l && r <= b) return dat[k]; // 範囲内
+        else {
+            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return {(vl.htor * vr.b + vr.htor) % vl.m, (vl.htol + vr.htol * vl.b) % vl.m, (vl.b * vr.b) % vl.m, vl.m};
+        }
+    }
+};
 
 int main(){
-    
+    int n, q; cin >> n >> q;
+    string s; cin >> s;
+    ll b = 29;
+
+    // 左からのハッシュ化, 右からのハッシュ化, 桁数を基数を用いて表現した数, mod
+    vector<RMQ<Data>> data(5, RMQ<Data>(n));
+    rep(i, 5) rep(j, n) data[i].update(j, {s[j]-'a'+1, s[j]-'a'+1, b, mod[i]});
+
+    rep(i, q){
+        int tmp; cin >> tmp;
+        if(tmp == 1){
+            int x; char c; cin >> x >> c; x--;
+            rep(i, 5) data[i].update(x, {c-'a'+1, c-'a'+1, b, mod[i]});
+        }
+        else if(tmp == 2){
+            int l, r; cin >> l >> r; l--, r--;
+            bool flag = true;
+            rep(i, 5) if(data[i].query(l, r+1).htor != data[i].query(l, r+1).htol) flag = false;
+            cout << (flag ? "Yes" : "No") << endl;
+        }
+    }
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -14,98 +14,70 @@
 #include <numeric>
 #include <iomanip>
 #include <functional>
+#include "atcoder/all"
+#include "atcoder/modint.hpp"
 using namespace std;
+using namespace atcoder;
 typedef long long ll;
 const int inf = 1e9;
 const ll infl = 1e18;
-#define rep(i,n) for (int i=0; i<n; i++)
+#define rep(i,n) for (ll i=0; i<n; i++)
 template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
 template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
 template<typename T> using min_priority_queue = priority_queue<T, vector<T>, greater<T>>;
 template<typename T> bool checker(T s, T t, T s__, T t__){ return ((s >= 0 && t >= 0 && s < s__ && t < t__) ? true : false); }
+template<typename T> T floor_multiple(T k, T m){ return k - ((k%m+m)%m); }
 using Graph = vector<vector<int>>;
+using mint = modint;
+
 /* RMQ：[0,n-1] について、区間ごとの最小値を管理する構造体
-    update(a,b,x): 区間[a,b) の要素に x を加算。O(log(n))
+    update(i,x): i 番目の要素を x に更新。O(log(n))
     query(a,b): [a,b) での最小の要素を取得。O(log(n))
 */
 template <typename T>
-class RMQ {
+class RMQtoR {
 public:
     const T INF = numeric_limits<T>::max();
-    int n;
-    vector<T> dat, lazy;
-    RMQ(int n_) : n(), dat(n_ * 4, 0), lazy(n_ * 4, 0) {
+    int n;         // 葉の数
+    vector<T> dat; // 完全二分木の配列
+    RMQtoR(int n_) : n(), dat(n_ * 4, 0) { // 葉の数は 2^x の形
         int x = 1;
-        while (n_ > x) x *= 2;
+        while (n_ > x) {
+            x *= 2;
+        }
         n = x;
     }
 
-    void update(int a, int b, T x, int k, int l, int r) {
-        eval(k);
-        if (a <= l && r <= b) {  // 完全に内側の時
-            lazy[k] = x;
-            eval(k);
-        } else if (a < r && l < b) {                     // 一部区間が被る時
-            update(a, b, x, k * 2 + 1, l, (l + r) / 2);  // 左の子
-            update(a, b, x, k * 2 + 2, (l + r) / 2, r);  // 右の子
-            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+    void update(int i, T x) {
+        i += n - 1;
+        dat[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2;  // parent
+            dat[i] = dat[i * 2 + 1].first * dat[i * 2 + 2].second + dat[i * 2 + 2].first;
         }
     }
-    void update(int a, int b, T x) { update(a, b, x, 0, 0, n); }
 
+    // the minimum element of [a,b)
+    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
     T query_sub(int a, int b, int k, int l, int r) {
-        eval(k);
-        if (r <= a || b <= l) {  // 完全に外側の時
-            return INF;
-        } else if (a <= l && r <= b) {  // 完全に内側の時
+        if (r <= a || b <= l) {
+            return {0, 0};
+        } else if (a <= l && r <= b) {
             return dat[k];
-        } else {  // 一部区間が被る時
+        } else {
             T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
             T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
-            return min(vl, vr);
+            return vl.first * vr.second + vl.second;
         }
-    }
-    T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
-
-private:
-    /* lazy eval */
-    void eval(int k) {
-        if (lazy[k] == 0) return;  // 更新するものが無ければ終了
-        if (k < n - 1) {             // 葉でなければ子に伝搬
-            lazy[k * 2 + 1] = lazy[k];
-            lazy[k * 2 + 2] = lazy[k];
-        }
-        // 自身を更新
-        dat[k] = lazy[k];
-        lazy[k] = 0;
     }
 };
 
 int main(){
-    int n; cin >> n;
-    vector<ll> a(n); rep(i, n) cin >> a[i];
+    int n, q; cin >> n >> q;
+    string s; cin >> s;
+    
+    mint::set_mod(998244353);
+    RMQtoR<pair<mint, int>> rmq_r(n);
 
-    RMQ<ll> rmq(n);
-    rep(i, n){
-        rmq.update(i, i+1, a[i]);
-    }
-
-    int q; cin >> q;
-    rep(i, q){
-        int tmp; cin >> tmp;
-        if(tmp == 1){
-            ll x; cin >> x;
-            rmq.update(0, n, x);
-        }
-        else if(tmp == 2){
-            ll i, x; cin >> i >> x;
-            i--;
-            rmq.update(i, i+1, rmq.query(i, i+1) + x);
-        }
-        else if(tmp == 3){
-            ll i; cin >> i; i--;
-            cout << rmq.query(i, i+1) << endl;
-        }
-    }
     return 0;
 }
